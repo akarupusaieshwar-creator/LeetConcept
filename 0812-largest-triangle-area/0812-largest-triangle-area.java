1aee91a970cd40136180640f856900e4aae20a10
akarupusaieshwar-creator <akarupusaieshwar@gmail.com>
2026-07-23 22:13:54 +0530

    Time: 10 ms (19.83%), Space: 46 MB (11.76%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.java
+++ 0812-largest-triangle-area/0812-largest-triangle-area.java
@@ -10,10 +10,10 @@ class Solution {
                     int x1 = points[i][0], y1 = points[i][1];
                     int x2 = points[j][0], y2 = points[j][1];
                     int x3 = points[k][0], y3 = points[k][1];
-                    // int a = (x1 * y2) + (x2 * y3);
-                    // int b = (x2 * y1) + (x3 * y2);
-                    // area += 0.5 * (a - b); 
-                    area = Math.abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))/2.0;
+                    int a = (x1 * y2) + (x2 * y3) + (x3 * y1);
+                    int b = (y1 * x2) + (y2 * x3) + (y3 * x1);
+                    area = 0.5 * Math.abs(a - b); 
+                    // area = Math.abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))/2.0;
                     max = Math.max(max,area);
                 }
             }
