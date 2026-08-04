dc78c42d091f3fd6411595a5aa209f7b48e068e3
Saieshwar akarupu <akarupusaieshwar@gmail.com>
2026-08-04 23:49:23 +0530

    Time: 11 ms (5.62%), Space: 44.7 MB (95.48%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    List<List<Integer>> al ;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        al = new ArrayList<>();
+        fun(nums,nums.length,0,new ArrayList<>());
+        // Collections.sort(al,(a,b)->{
+        //     int s = Math.min(a.size(),b.size());
+        //     for(int i=0;i<s;i++){
+        //         if(!a.get(i).equals(b.get(i))){
+        //             return Integer.compare(a.get(i),b.get(i));
+        //         }
+        //     }
+        //     return Integer.compare(a.size(),b.size());
+        // });
+        // Collections.sort(al,(a,b)->{
+        //     int i=0;
+        //     while(i < a.size() && i < b.size()){
+        //         if(!a.get(i).equals(b.get(i))){
+        //             return a.get(i) - b.get(i);
+        //         }
+        //         i++;
+        //     }
+        //     return a.size() - b.size();
+        // });
+        return al;
+    }
+    public void fun(int[] arr,int n,int idx,ArrayList<Integer> list){
+        if(idx == n){
+            if(!al.contains(list)){
+                al.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        list.add(arr[idx]);
+        // Collections.sort(list);
+        fun(arr,n,idx+1,list);
+        list.remove(list.size()-1);
+        fun(arr,n,idx+1,list);
+    }
+}
