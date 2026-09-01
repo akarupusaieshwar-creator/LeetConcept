e8e2b5810b5612064b84ff47d66da3f2f1e5b78a
Saieshwar akarupu <akarupusaieshwar@gmail.com>
2026-09-01 13:51:38 +0530

    Time: 10 ms (8.24%), Space: 45 MB (76.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,30 +2,27 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        for(int i=0;i<n;i++){
-            nums1[i] = fun(nums2,nums1[i]);
-        }
+        int[] t = Arrays.copyOf(nums2,m);
         Stack<Integer> st = new Stack<>();
         for(int i=0;i<m;i++){
             while(!st.isEmpty() && nums2[st.peek()] < nums2[i]){
-                nums2[st.pop()] = nums2[i];
+                t[st.pop()] = nums2[i];
             }
             st.push(i);
         }
         while(!st.isEmpty()){
-            nums2[st.pop()] = -1;
+            t[st.pop()] = -1;
         }
+        System.out.println(Arrays.toString(t));
+        int[] r = new int[n];
         for(int i=0;i<n;i++){
-            nums1[i] = nums2[nums1[i]];
-        }
-        return nums1;
-    }
-    public int fun(int[] arr,int k){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] == k){
-                return i;
+            for(int j=0;j<m;j++){
+                if(nums1[i] == nums2[j]){
+                    r[i] = t[j];
+                    // System.out.println(t[j] + " "+r[i]);
+                }
             }
         }
-        return -1;
+        return r;
     }
 }
