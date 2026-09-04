cea97838b05f34d9fd7337f7f061e37e6ba166b2
Saieshwar akarupu <akarupusaieshwar@gmail.com>
2026-09-04 12:05:12 +0530

    Time: 1 ms (99.79%), Space: 46.6 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,23 +1,26 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int m = nums[0];
         int n = nums.length;
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            m = Math.max(m,nums[i]);
-            arr[i] = m;
+        int[] max = new int[n];
+        int[] min = new int[n];
+        int mx = nums[0];
+        int mn = nums[n-1];
+        max[0] = nums[0];
+        min[n-1] = nums[n-1];
+        for(int i=1;i<n;i++){
+            mx = Math.max(mx,nums[i]);
+            max[i] = mx;
         }
-        m = arr[n-1];
-        for(int i=n-1;i>=0;i--){
-            m = Math.min(m,nums[i]);
-            arr[i] -= m;
+        for(int i=n-2;i>=0;i--){
+            mn = Math.min(mn,nums[i]);
+            min[i] = mn;
         }
         for(int i=0;i<n;i++){
-            if(arr[i] <= k){
-                return i;
+            if(max[i] - min[i] <= k){
+                 return i;
             }
+            
         }
-        // System.out.println(Arrays.toString(arr));
         return -1;
     }
 }
